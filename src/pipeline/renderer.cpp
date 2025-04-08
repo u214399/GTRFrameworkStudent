613fecf73c3d936f250fc8465d051675dca4727f
u214399 <jiaheng.huang01@estudiant.upf.edu>
2025-04-08 20:18:49 +0200

    3.3

---
+++ src/pipeline/renderer.cpp
@@ -0,0 +1,308 @@
+#include "renderer.h"
+
+#include <algorithm> //sort
+
+#include "camera.h"
+#include "../gfx/gfx.h"
+#include "../gfx/shader.h"
+#include "../gfx/mesh.h"
+#include "../gfx/texture.h"
+#include "../gfx/fbo.h"
+#include "../pipeline/prefab.h"
+#include "../pipeline/material.h"
+#include "../pipeline/animation.h"
+#include "../utils/utils.h"
+#include "../extra/hdre.h"
+#include "../core/ui.h"
+#include "../pipeline/light.h"
+
+#include "scene.h"
+
+
+using namespace SCN;
+
+//some globals
+GFX::Mesh sphere;
+
+Renderer::Renderer(const char* shader_atlas_filename)
+{
+	render_wireframe = false;
+	render_boundaries = false;
+	scene = nullptr;
+	skybox_cubemap = nullptr;
+
+	if (!GFX::Shader::LoadAtlas(shader_atlas_filename))
+		exit(1);
+	GFX::checkGLErrors();
+
+	sphere.createSphere(1.0f);
+	sphere.uploadToVRAM();
+}
+
+void Renderer::setupScene()
+{
+	if (scene->skybox_filename.size())
+		skybox_cubemap = GFX::Texture::Get(std::string(scene->base_folder + "/" + scene->skybox_filename).c_str());
+	else
+		skybox_cubemap = nullptr;
+}
+
+void Renderer::parseNodes(SCN::Node* node, Camera* cam) {
+	if (!node) return;
+
+
+
+	if(node->mesh){
+		Matrix44 global_matrix = node->getGlobalMatrix();
+		float distance = cam->eye.distance(global_matrix.getTranslation());
+		bool must_render = true;
+		must_render &= (distance < 30);
+		Vector3f bb_center = global_matrix * node->aabb.center;
+		Vector3f bb_halfsize = global_matrix * node->aabb.halfsize;
+		cam->extractFrustum();
+		must_render &= (cam->testBoxInFrustum(bb_center, bb_halfsize) != CLIP_OUTSIDE);
+		if (must_render) {
+			sDrawCommand values;
+			values.mesh = node->mesh;
+			values.model = node->getGlobalMatrix();
+			values.material = node->material;
+			values.distance = distance;
+			if (values.material->alpha_mode == BLEND) {
+				transparent_to_render.push_back(values);
+				std::sort(transparent_to_render.begin(), transparent_to_render.end(), [](const sDrawCommand& s1, const sDrawCommand& s2) {
+					return s1.distance > s2.distance;
+					});
+
+			}
+			else {
+				entities_to_render.push_back(values);
+				std::sort(entities_to_render.begin(), entities_to_render.end(), [](const sDrawCommand& s1, const sDrawCommand& s2) {
+					return s1.distance < s2.distance;
+					});
+			}
+		}
+	}
+
+	for (SCN::Node* child : node->children) {
+		parseNodes(child , cam);
+	}
+
+}
+
+void Renderer::parseSceneEntities(SCN::Scene* scene, Camera* cam) {
+	// HERE =====================
+	// TODO: GENERATE RENDERABLES
+	// ==========================
+
+	entities_to_render.clear();
+	transparent_to_render.clear();
+	light_list.clear();
+	for (int i = 0; i < scene->entities.size(); i++) {
+		BaseEntity* entity = scene->entities[i];
+
+		if (!entity->visible) {
+			continue;
+		}
+
+		
+		if (entity->getType() == eEntityType::PREFAB) {
+			parseNodes(&((PrefabEntity*)entity)->root, cam);
+		}
+		else if (entity->getType() == eEntityType::LIGHT) light_list.push_back( (LightEntity*) entity);
+
+		// Store Lights
+		// ...
+	}
+	
+}
+
+void Renderer::renderScene(SCN::Scene* scene, Camera* camera)
+{
+	this->scene = scene;
+	setupScene();
+
+	parseSceneEntities(scene, camera);
+
+	//set the clear color (the background color)
+	glClearColor(scene->background_color.x, scene->background_color.y, scene->background_color.z, 1.0);
+
+	// Clear the color and the depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	GFX::checkGLErrors();
+
+	//render skybox
+	if(skybox_cubemap)
+		renderSkybox(skybox_cubemap);
+
+	// HERE =====================
+	// TODO: RENDER RENDERABLES
+	// ==========================
+	
+
+
+	
+	for(sDrawCommand draw : entities_to_render){
+		renderMeshWithMaterial(draw.model, draw.mesh, draw.material);
+	}
+
+
+	for (sDrawCommand draw : transparent_to_render) {
+		renderMeshWithMaterial(draw.model, draw.mesh, draw.material);
+	}
+
+
+}
+
+
+void Renderer::renderSkybox(GFX::Texture* cubemap)
+{
+	Camera* camera = Camera::current;
+
+	// Apply skybox necesarry config:
+	// No blending, no dpeth test, we are always rendering the skybox
+	// Set the culling aproppiately, since we just want the back faces
+	glDisable(GL_BLEND);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+
+	if (render_wireframe)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	GFX::Shader* shader = GFX::Shader::Get("skybox");
+	if (!shader)
+		return;
+	shader->enable();
+
+	// Center the skybox at the camera, with a big sphere
+	Matrix44 m;
+	m.setTranslation(camera->eye.x, camera->eye.y, camera->eye.z);
+	m.scale(10, 10, 10);
+	shader->setUniform("u_model", m);
+
+	// Upload camera uniforms
+	shader->setUniform("u_viewprojection", camera->viewprojection_matrix);
+	shader->setUniform("u_camera_position", camera->eye);
+
+	shader->setUniform("u_texture", cubemap, 0);
+
+	sphere.render(GL_TRIANGLES);
+
+	shader->disable();
+
+	// Return opengl state to default
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glEnable(GL_DEPTH_TEST);
+}
+
+// Renders a mesh given its transform and material
+void Renderer::renderMeshWithMaterial(const Matrix44 model, GFX::Mesh* mesh, SCN::Material* material)
+{
+	//in case there is nothing to do
+	if (!mesh || !mesh->getNumVertices() || !material )
+		return;
+    assert(glGetError() == GL_NO_ERROR);
+
+	//define locals to simplify coding
+	GFX::Shader* shader = NULL;
+	Camera* camera = Camera::current;
+
+	glEnable(GL_DEPTH_TEST);
+
+	//chose a shader
+	shader = GFX::Shader::Get("texture");
+
+    assert(glGetError() == GL_NO_ERROR);
+
+	//no shader? then nothing to render
+	if (!shader)
+		return;
+	shader->enable();
+
+	material->bind(shader);
+
+
+
+	//Sending the lights
+	vec3* light_pos = new vec3[light_list.size()];
+	vec3* light_color = new vec3[light_list.size()];
+	float* light_intensity = new float[light_list.size()];
+	vec3* light_dir = new vec3[light_list.size()];
+	int* light_type = new int[light_list.size()];
+	float alpha_min;
+	float alpha_max;
+	int i = 0u;
+	for (LightEntity* light : light_list) {
+		light_pos[i] = light->root.getGlobalMatrix().getTranslation();
+		light_intensity[i] = light->intensity;
+		light_color[i] = light->color;
+		light_dir[i] = light->root.model.frontVector();
+		light_type[i] = light->light_type;
+		if (light->light_type == 2){
+			alpha_min = light->cone_info.x;
+			alpha_max = light->cone_info.y;
+		}
+		i++;
+	}
+
+
+	shader->setUniform3Array("u_light_pos", (float*)light_pos, min(light_list.size(), 10));
+	shader->setUniform3Array("u_light_color", (float*)light_color, min(light_list.size(), 10));
+	shader->setUniform1Array("u_light_intensity", (float*)light_intensity, min(light_list.size(), 10));
+	shader->setUniform3Array("u_light_dir", (float*)light_dir, min(light_list.size(), 10));
+	shader->setUniform1Array("u_type", light_type, min(light_list.size(), 10));
+	shader->setUniform("u_alpha_min", alpha_min);
+	shader->setUniform("u_alpha_max", alpha_max);
+
+
+	float shininnes = 5;
+	shader->setUniform("u_shine", shininnes);
+	shader->setUniform("u_ambient_light", Scene::instance->ambient_light);
+
+
+	delete[] light_pos;
+	delete[] light_color;
+	delete[] light_intensity;
+	delete[] light_dir;
+	delete[] light_type;
+
+	//upload uniforms
+	shader->setUniform("u_model", model);
+
+	// Upload camera uniforms
+	shader->setUniform("u_viewprojection", camera->viewprojection_matrix);
+	shader->setUniform("u_camera_position", camera->eye);
+
+	// Upload time, for cool shader effects
+	float t = getTime();
+	shader->setUniform("u_time", t );
+
+	// Render just the verticies as a wireframe
+	if (render_wireframe)
+		glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+
+	//do the draw call that renders the mesh into the screen
+	mesh->render(GL_TRIANGLES);
+
+	//disable shader
+	shader->disable();
+
+	//set the render state as it was before to avoid problems with future renders
+	glDisable(GL_BLEND);
+	glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+}
+
+#ifndef SKIP_IMGUI
+
+void Renderer::showUI()
+{
+		
+	ImGui::Checkbox("Wireframe", &render_wireframe);
+	ImGui::Checkbox("Boundaries", &render_boundaries);
+
+	//add here your stuff
+	//...
+}
+
+#else
+void Renderer::showUI() {}
+#endif
